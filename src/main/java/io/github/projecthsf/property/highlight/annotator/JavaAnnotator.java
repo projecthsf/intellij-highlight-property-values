d8379b7bb014f2fb3b50b1007dd38200fcb3e475
linhnnmt <162856460+linhnnmt@users.noreply.github.com>
2025-08-18 07:45:41 +0700

    Update JavaAnnotator.java

--- src/main/java/io/github/projecthsf/property/highlight/annotator/JavaAnnotator.java
+++ src/main/java/io/github/projecthsf/property/highlight/annotator/JavaAnnotator.java
@@ -2,23 +2,12 @@ package io.github.projecthsf.property.highlight.annotator;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLiteralValue;
+import io.github.projecthsf.property.highlight.enums.LanguageEnum;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaAnnotator extends CommonAnnotator {
     @Override
-    protected String getLiteralValue(@NotNull PsiElement element) {
-        if (!(element instanceof PsiLiteralValue literalExpression)) {
-            return null;
-        }
-
-        if (literalExpression.getValue() == null) {
-            return null;
-        }
-
-        if (literalExpression.getValue() instanceof Boolean) {
-            return null;
-        }
-
-        return literalExpression.getValue() instanceof String ? (String) literalExpression.getValue() : null;
+    protected LanguageEnum getLanguageEnum() {
+        return LanguageEnum.JAVA;
     }
 }
